9d5be8986f8175d623a016977b9b943bd1bc9264
todashuta <toda.shuta@gmail.com>
2025-03-25 23:31:47 +0900

    update

--- localview_with_confirm.py
+++ localview_with_confirm.py
@@ -18,10 +18,10 @@
 
 
 bl_info = {
-    "name": "Safe Localview",
+    "name": "Local View with Confirm",
     "author": "todashuta",
-    "version": (0, 0, 4),
-    "blender": (3, 3, 0),
+    "version": (0, 0, 5),
+    "blender": (3, 6, 0),
     "location": "-",
     "description": "-",
     "warning": "",
@@ -34,37 +34,36 @@ bl_info = {
 import bpy
 
 
-def get_addon_prefs():
-    addon_prefs = bpy.context.preferences.addons[__name__].preferences
+def get_addon_prefs(context):
+    addon_prefs = context.preferences.addons[__name__].preferences
     return addon_prefs
 
 
-shading_types = {
-        "WIREFRAME": "Wireframe",
-        "SOLID": "Solid",
-}
-
-
-class SafeLocalviewOperator(bpy.types.Operator):
-    bl_idname = "view3d.safe_localview"
-    bl_label = "Safe Localview"
+class LocalviewWithConfirmOperator(bpy.types.Operator):
+    bl_idname = "view3d.localview_with_confirm"
+    bl_label = "Local View with Confirm"
 
     @classmethod
     def poll(cls, context):
         return bpy.ops.view3d.localview.poll()
 
     def execute(self, context):
-        prefs = get_addon_prefs()
-        in_localview = context.space_data.local_view is not None
-        shading_type = context.space_data.shading.type
-        if (in_localview and
-                shading_type in {'MATERIAL', 'RENDERED'}):
-            t = prefs.preferred_shading_type
-            context.space_data.shading.type = t
-            msg = f"Viewport Shading changed to {shading_types[t]}"
-            self.report({"INFO"}, msg)
-
+        prefs = get_addon_prefs(context)
         return bpy.ops.view3d.localview(frame_selected=prefs.frame_selected)
+
+    def invoke(self, context, event):
+        if context.space_data.local_view is None:
+            return self.execute(context)
+        shading_type = context.space_data.shading.type
+        modes = {
+                "SOLID":    {"SOLID", "MATERIAL", "RENDERED"},
+                "MATERIAL": {         "MATERIAL", "RENDERED"},
+                "RENDERED": {                     "RENDERED"},
+        }[context.scene.LocalViewWithConfirmAddon_modes]
+        if shading_type in modes:
+            wm = context.window_manager
+            return wm.invoke_confirm(self, event)
+        return self.execute(context)
 
 
 def auto_rebind(self, context):
@@ -72,15 +71,8 @@ def auto_rebind(self, context):
     register_keymaps()
 
 
-class SafeLocalviewPreferences(bpy.types.AddonPreferences):
+class LocalviewWithConfirmPreferences(bpy.types.AddonPreferences):
     bl_idname = __name__
-
-    items = (
-            ("WIREFRAME", "Wireframe", ""),
-            ("SOLID",     "Solid",     ""))
-    preferred_shading_type: bpy.props.EnumProperty(
-            name="Preferred Shading Type",
-            items=items)
 
     frame_selected: bpy.props.BoolProperty(
             name="Frame Selected",
@@ -89,20 +81,19 @@ class SafeLocalviewPreferences(bpy.types.AddonPreferences):
 
     use_shortcut: bpy.props.BoolProperty(
             name="Use Default Shortcut",
-            default=True,
+            default=False,
             update=auto_rebind)
 
     def draw(self, context):
         layout = self.layout
         row = layout.row()
-        row.prop(self, "preferred_shading_type")
         row.prop(self, "frame_selected")
         row.prop(self, "use_shortcut")
 
 
 addon_keymaps = []
 def register_keymaps():
-    prefs = get_addon_prefs()
+    prefs = get_addon_prefs(bpy.context)
     if not prefs.use_shortcut:
         return
 
@@ -111,28 +102,43 @@ def register_keymaps():
         return
 
     km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
-    kmi = km.keymap_items.new(SafeLocalviewOperator.bl_idname, "SLASH", "PRESS")
+    kmi = km.keymap_items.new(LocalviewWithConfirmOperator.bl_idname, "SLASH", "PRESS")
     addon_keymaps.append((km, kmi))
 
     km = kc.keymaps.new(name="3D View", space_type="VIEW_3D")
-    kmi = km.keymap_items.new(SafeLocalviewOperator.bl_idname, "NUMPAD_SLASH", "PRESS")
+    kmi = km.keymap_items.new(LocalviewWithConfirmOperator.bl_idname, "NUMPAD_SLASH", "PRESS")
     addon_keymaps.append((km, kmi))
 
 def unregister_keymaps():
-    for km,kmi in addon_keymaps:
+    for km, kmi in addon_keymaps:
         km.keymap_items.remove(kmi)
     addon_keymaps.clear()
 
 
 classes = (
-        SafeLocalviewPreferences,
-        SafeLocalviewOperator,
+        LocalviewWithConfirmPreferences,
+        LocalviewWithConfirmOperator,
 )
 
 
+def menu_func(self, context):
+    layout = self.layout
+    layout.prop(context.scene, "LocalViewWithConfirmAddon_modes")
+
+
 def register():
+    bpy.types.Scene.LocalViewWithConfirmAddon_modes = bpy.props.EnumProperty(
+            name="LVWC Modes",
+            description="Local View with Confirm Modes",
+            default="MATERIAL",
+            items=[("SOLID",    "Solid / Material Preview / Rendered", ""),
+                   ("MATERIAL", "Material Preview / Rendered",         ""),
+                   ("RENDERED", "Rendered",                            "")])
+
     for cls in classes:
         bpy.utils.register_class(cls)
+
+    bpy.types.VIEW3D_PT_shading.append(menu_func)
 
     register_keymaps()
 
@@ -140,8 +146,13 @@ def register():
 def unregister():
     unregister_keymaps()
 
+    bpy.types.VIEW3D_PT_shading.remove(menu_func)
+
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
+
+    if hasattr(bpy.types.Scene, "LocalViewWithConfirmAddon_modes"):
+        del bpy.types.Scene.LocalViewWithConfirmAddon_modes
 
 
 if __name__ == "__main__":
